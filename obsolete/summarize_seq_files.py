72deaf6bcecedea6c0f9dd36c76190ac1fcb5777
Keith Arora-Williams <karoraw1@jhu.edu@login-node03.cm.cluster>
2017-12-15 11:12:22 -0500

    began work on file locator

--- obsolete/summarize_seq_files.py
+++ obsolete/summarize_seq_files.py
@@ -7,5 +7,12 @@ Created on Thu Dec 14 19:25:38 2017
 """
 
 import pandas as pd
+import numpy as np
+import os
 
 new_master_f = 'Sampling_processing_worksheet_121217.csv'
+print "Metadata sheet exists:", os.path.exists(new_master_f)
+metadata_df = pd.read_csv(new_master_f, parse_dates=False)
+print "\tShape is:", metadata_df.shape
+
+print metadata_df.columns
