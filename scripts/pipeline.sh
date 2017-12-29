e48ab58bfe51e6c0e33e095c9a196fcfd6119a17
Keith Arora-Williams <karoraw1@jhu.edu@login-node01.cm.cluster>
2017-12-29 13:24:32 -0500

    parallelizing demux commands, making skeleton pipeline

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -14,13 +14,24 @@
 module load R/3.4.0
 module load python/2.7.10
 
+RAW_BASE=/data/sprehei1/Raw_data_group
+SEQ_ID=esakows1_132789
+RAW_FWD=Undetermined_S0_L001_R1_001.fastq
+RAW_REV=Undetermined_S0_L001_R2_001.fastq
+RAW_IDX=Undetermined_S0_L001_I1_001.fastq
 
-BASE_DIR=@#$
-
+BASE_OUT=/home-3/karoraw1@jhu.edu/scratch/16S_Libraries
 
 # demultiplex 
-for i in `cat ./Barcode_sequences.txt`; do grep -B1 "$i" Undetermined_S0_L001_I1_001.fastq > "$i".ids.fastq; done
+DEMUX_DIR=$BASE_OUT/$SEQ_ID/Demux
+mkdir -p $DEMUX_DIR
 
+parallel -j 24 -a ../data/Barcode_sequences.txt "grep -B1 {} $RAW_BASE/$SEQ_ID/$RAW_IDX > $DEMUX_DIR/{}.ids.fastq"
+ls $DEMUX_DIR/*.ids.fastq | parallel -j 24 "grep -A0 @M00776 {} >> {}.headers.txt"
+
+rm *.ids.fastq
+for i in ./*.headers.txt; do cat "$i" | sed 's/1:N:0:0//g' > Barcode_IDs/"$i".2.txt; done
+rm *.headers.txt
 
 # combine paired end reads
 mkdir Flash_Files
