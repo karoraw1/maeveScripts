e1312d0babe01ccb84894ab3e025267071345935
Keith Arora-Williams <karoraw1@jhu.edu@bigmem0043.cm.cluster>
2018-01-01 14:01:16 -0500

    plotting script v1

--- scripts/quality_plot.R
+++ scripts/quality_plot.R
@@ -1,13 +1,15 @@
-
+library(dada2)
 args <- commandArgs(TRUE)
 base_path <- as.double(args[1])
 seq_ID <- as.double(args[2])
 
-
-path <- paste(base_path
+path=file.path(base_path, seq_ID, "Demux")
 fnFs <- sort(list.files(path, pattern=".R1.fastq", full.names = TRUE))
 fnRs <- sort(list.files(path, pattern=".R2.fastq", full.names = TRUE))
+sample.names <- sapply(strsplit(basename(fnFs), ".headers"), `[`, 1)
 
+fwdPNGname = file.path(base_path, seq_ID, "R1_quals.png")
+revPNGname = file.path(base_path, seq_ID, "R2_quals.png")
 
 png(filename=fwdPNGname)
 plotQualityProfile(fnFs[1:2])
@@ -15,4 +17,4 @@ dev.off()
 
 png(filename=revPNGname)
 plotQualityProfile(fnRs[1:2])
-dev.off
+dev.off()
