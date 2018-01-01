3de8c2a0d303b940a0a4596f9d4602cd98859a13
Keith Arora-Williams <karoraw1@jhu.edu@login-node01.cm.cluster>
2018-01-01 14:31:58 -0500

    use the biggest sample for quality plots

--- scripts/quality_plot.R
+++ scripts/quality_plot.R
@@ -3,18 +3,29 @@ args <- commandArgs(TRUE)
 base_path <- as.double(args[1])
 seq_ID <- as.double(args[2])
 
+# library(dada2)
+# base_path = "/home-3/karoraw1@jhu.edu/scratch/16S_Libraries"
+# seq_ID = "esakows1_132789"
+
 path=file.path(base_path, seq_ID, "Demux")
 fnFs <- sort(list.files(path, pattern=".R1.fastq", full.names = TRUE))
 fnRs <- sort(list.files(path, pattern=".R2.fastq", full.names = TRUE))
 sample.names <- sapply(strsplit(basename(fnFs), ".headers"), `[`, 1)
 
+
 fwdPNGname = file.path(base_path, seq_ID, "R1_quals.png")
 revPNGname = file.path(base_path, seq_ID, "R2_quals.png")
 
+file_sizes_f = sort(sapply(fnFs, file.size), decreasing = T, na.last = NA)
+file_sizes_r = sort(sapply(fnRs, file.size), decreasing = T, na.last = NA)
+to_plot_fwd=row.names(as.data.frame(file_sizes_f[1:2]))
+to_plot_rev=row.names(as.data.frame(file_sizes_r[1:2]))
+
+
 png(filename=fwdPNGname)
-plotQualityProfile(fnFs[1:2])
+plotQualityProfile(to_plot_fwd)
 dev.off()
 
 png(filename=revPNGname)
-plotQualityProfile(fnRs[1:2])
+plotQualityProfile(to_plot_rev)
 dev.off()
